30f48b7edbd2a5a0d61d983472b162b9aec68e31
jakkula007 <145642340+jakkula007@users.noreply.github.com>
2024-01-09 13:30:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,12 +11,12 @@ terraform {
 }
 
 provider "azurerm" {
-  features {}
-
+  features      = {}
+  subscription_id = var.subscription_id
   client_id       = var.client_id
   client_secret   = var.client_secret
-  subscription_id = var.subscription_id
   tenant_id       = var.tenant_id
+  # other provider configuration...
 }
 
 # This retrieves information about an existing Azure resource group named "AUE-KOR-ResourceGroup-TRN01"
