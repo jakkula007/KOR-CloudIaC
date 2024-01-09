e1ca694748d634e883cc03284fffc6a56f1a5849
jakkula007 <145642340+jakkula007@users.noreply.github.com>
2024-01-09 13:59:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,47 +1,51 @@
-#This section configures the Azure provider for Terraform. 
-#It sets up the necessary credentials (client ID, client secret, subscription ID, and tenant ID) 
-#to authenticate with Azure.
+# This section configures the Azure provider for Terraform.
+# It sets up the necessary credentials (client ID, client secret, subscription ID, and tenant ID)
+# to authenticate with Azure.
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.85.0"
+    }
+  }
+}
 
 provider "azurerm" {
   features {}
+
   client_id       = var.client_id
   client_secret   = var.client_secret
   subscription_id = var.subscription_id
   tenant_id       = var.tenant_id
 }
 
-
-#This retrieves information about an existing Azure resource group named "AUE-KOR-ResourceGroup-TRN01" 
-#using the azurerm_resource_group data source.
-
+# This retrieves information about an existing Azure resource group named "AUE-KOR-ResourceGroup-TRN01"
+# using the azurerm_resource_group data source.
 data "azurerm_resource_group" "existing" {
   name = "AUE-KOR-ResourceGroup-TRN01"
 }
 
-#This resource block creates an Azure Virtual Network named "jack-network" with the address space "10.0.0.0/16" 
-#in the specified resource group and location.
-
+# This resource block creates an Azure Virtual Network named "jack-network" with the address space "10.0.0.0/16"
+# in the specified resource group and location.
 resource "azurerm_virtual_network" "example" {
-  name                = "virtual_network_name"
+  name                = "jack-virtual-network"
   resource_group_name = data.azurerm_resource_group.existing.name
   location            = data.azurerm_resource_group.existing.location
   address_space       = ["10.0.0.0/16"]
 }
 
 resource "azurerm_subnet" "public_subnet" {
-  count                = 1
-  name                 = "public-subnet-${count.index + 1}"
+  name                 = "public-subnet"
   resource_group_name  = data.azurerm_resource_group.existing.name
   virtual_network_name = azurerm_virtual_network.example.name
-  address_prefixes     = ["10.0.${count.index + 1}.0/24"]
+  address_prefixes     = ["10.0.1.0/24"]
 }
 
 resource "azurerm_subnet" "private_subnet" {
-  count                = 1
   name                 = "private-subnet"
   resource_group_name  = data.azurerm_resource_group.existing.name
   virtual_network_name = azurerm_virtual_network.example.name
-  address_prefixes     = ["10.0.${count.index + 4}.0/24"]
+  address_prefixes     = ["10.0.4.0/24"]
 }
 
 
@@ -56,27 +60,27 @@ resource "azurerm_subnet" "private_subnet" {
 # It also links to a public IP from the list created earlier.
 
 resource "azurerm_network_interface" "example" {
-  count               = 1
-  name                = "jack-nic"
+  count               = 3
+  name                = "jack-nic-${count.index + 1}"
   resource_group_name = data.azurerm_resource_group.existing.name
   location            = data.azurerm_resource_group.existing.location
 
   ip_configuration {
     name      = "jack-ip-config"
-    subnet_id = azurerm_subnet.private_subnet[count.index % length(azurerm_subnet.private_subnet)].id
-
+    subnet_id = azurerm_subnet.private_subnet.id
     private_ip_address_allocation = "Dynamic"
-    
+    public_ip_address_id          = azurerm_public_ip.example[count.index].id
   }
 }
+
 
 
 #This resource block creates Azure public IP addresses for each virtual machine with dynamic allocation.
 
 # Create Public IP addresses
 resource "azurerm_public_ip" "example" {
-  count               = 1
-  name                = "example-public-ip"
+  count               = 3
+  name                = "example-public-ip-${count.index + 1}"
   resource_group_name = data.azurerm_resource_group.existing.name
   location            = data.azurerm_resource_group.existing.location
   allocation_method   = "Dynamic"
@@ -130,7 +134,7 @@ resource "azurerm_network_security_rule" "example_outbound_rule" {
 #This resource block associates the NSG with the network interface of each virtual machine.
 
 resource "azurerm_network_interface_security_group_association" "example_nic_nsg_association" {
-  count = 1
+  count = 3  # Update this count accordingly
 
   network_interface_id      = azurerm_network_interface.example[count.index % length(azurerm_network_interface.example)].id
   network_security_group_id = azurerm_network_security_group.example_nsg.id
@@ -164,76 +168,40 @@ resource "azurerm_network_security_rule" "example_rdp_rule" {
 #sets the VM size based on user input or predefined values, and uses the specified OS type.
 
 
-resource "azurerm_virtual_machine" "example" {
 
-  count                 = 1
-  name                  = "jack-virtual_machine_name"
+
+  resource "azurerm_virtual_machine" "example" {
+  count                 = 3
+  name                  = "jack-virtual_machine_name-${count.index + 1}"
+
   vm_size               = "Standard_D2_v2"
   resource_group_name   = data.azurerm_resource_group.existing.name
   location              = data.azurerm_resource_group.existing.location
   network_interface_ids = [azurerm_network_interface.example[count.index].id]
-  
-    
-  
 
+  storage_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-Datacenter"
+    version   = "latest"
+  }
 
-
-
-
-  #This specifies the image reference for the virtual machine, 
-  #determining the OS image to use based on the OS type specified.
-
-
-  /* publisher:
-For Windows OS, it sets the publisher to "MicrosoftWindowsServer."
-For Linux OS, it sets the publisher to "Canonical."
-The publisher represents the entity that created the OS image.
-
-
-offer:
-For Windows OS, it sets the offer to "WindowsServer."
-For Linux OS, it sets the offer to "UbuntuServer."
-The offer is a specific edition or version of the OS.
-
-
-sku:
-For Windows OS, it sets the SKU (stock keeping unit) to "2019-Datacenter."
-For Linux OS, it sets the SKU to "18.04-LTS."
-The SKU specifies the particular variant of the OS.
-
-version:
-It sets the version to "latest," indicating that the latest available version of the specified OS should be used.*/
-
-   storage_image_reference {
-	publisher = "MicrosoftWindowsServer"
-	offer     = "WindowsServer"
-	sku       = "2019-Datacenter"
-	version   = "latest"
-}
-
-   os_profile {
+  os_profile {
     computer_name  = "jackcomp"
     admin_username = "jackuser"
     admin_password = "Password123"
-}
-
+  }
 
   os_profile_windows_config {
     provision_vm_agent = true
   }
 
-  #This defines the OS disk for the virtual machine, specifying details like name, caching, 
-  #creation options, managed disk type, disk size, and OS type.
-
   storage_os_disk {
-    name              = "CentOS121-os-disk-vm"
+    name              = "osdisk-${count.index + 1}"
     caching           = "ReadWrite"
     create_option     = "FromImage"
     managed_disk_type = "Standard_LRS"
     disk_size_gb      = 128
-
-    
-
   }
 
   tags = {
